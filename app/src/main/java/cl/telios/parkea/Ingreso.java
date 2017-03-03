66b0a5cdf5a8c0bd60efec4b6e6d16b835469d56
Ruben Rojas <rubro1991@gmail.com>
2017-03-03 18:14:45 -0300

    -- [2017-03-03 18:14]

--- app/src/main/java/cl/telios/parkea/Ingreso.java
+++ app/src/main/java/cl/telios/parkea/Ingreso.java
@@ -1,14 +1,66 @@
 package cl.telios.parkea;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.icu.text.DateFormat;
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Date;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+import static cl.telios.parkea.R.id.start;
+import static cl.telios.parkea.R.id.textView;
 
 public class Ingreso extends AppCompatActivity {
-
+    TextView hora;
+    Button continuar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/OpenSans-Regular.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingreso);
-        getSupportActionBar().setTitle("ingreso");
+        getSupportActionBar().hide();
+        hora = (TextView) findViewById(R.id.hora);
+        String minutos = "";
+        if(new Date().getMinutes()<10){
+            minutos = "0"+String.valueOf(new Date().getMinutes());
+        }
+        else{
+            minutos = String.valueOf(new Date().getMinutes());
+        }
+        String horas = String.valueOf(new Date().getHours());
+        String currentDateTimeString = horas+":"+minutos;
+        hora.setText(currentDateTimeString);
+
+        continuar = (Button)findViewById(R.id.continuar);
+        continuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Ingreso.this, Main.class);
+                startActivity(intent);
+                Toast.makeText(Ingreso.this, "Ingreso Registrado", Toast.LENGTH_SHORT).show();
+                Ingreso.this.finish();
+            }
+        });
+
+    }
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
--- app/src/main/java/cl/telios/parkea/Salida.java
+++ app/src/main/java/cl/telios/parkea/Salida.java
@@ -1,14 +1,43 @@
 package cl.telios.parkea;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class Salida extends AppCompatActivity {
-
+    Button salida;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/OpenSans-Regular.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_salida);
-        getSupportActionBar().setTitle("salida");
+        getSupportActionBar().hide();
+
+        salida = (Button)findViewById(R.id.salida);
+        salida.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Salida.this, Main.class);
+                startActivity(intent);
+                Toast.makeText(Salida.this, "Salida Registrada", Toast.LENGTH_SHORT).show();
+                Salida.this.finish();
+            }
+        });
     }
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
 }
